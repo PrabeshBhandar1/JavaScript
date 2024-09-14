92888457e88b54ea8f4a9908218105d37d927579
Prabesh Bhandari <162716493+PrabeshBhandar1@users.noreply.github.com>
2024-09-14 08:14:46 +0000

    change

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 13422
+let accountEmail = "prabesh@google.com"
+var accountPassword = "2314"
+accountCity = "Sindhuli"
+
+//accountId = 2// not allowed to change beacause of const
+
+accountEmail = "ram@test.com"
+accountPassword = "6526587"
+accountCity = "Ktm"
+
+
+
+console.table([accountEmail, accountPassword, accountCity])
+
+
+/* prefer not to use var because of issue in block scope
+    and functional scope  
+    */
+
+
+
+
+
+
+    
---
+++ 01_basic/datatypes.js
@@ -0,0 +1,26 @@
+"use strict";     // treat all js code as newer version
+
+
+//  alert("hello")   // We are using nodejs not browser
+
+let name = "prabesh"
+let age = 24
+let isLoggedIn = false
+let state;
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol  => unique
+
+
+
+
+// object 
+
+console.log(typeof undefined) 
